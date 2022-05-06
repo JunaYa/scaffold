ea731789fed053320125d8a7066e73149afcac15
YajunWang <wangyajun@hetao101.com>
2022-05-06 19:23:26 +0800

    feat: update page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
-import legacy from '@vitejs/plugin-legacy'
 // https://vitejs.dev/config/
 export default defineConfig({
   // 项目根目录
@@ -25,9 +24,6 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    // legacy({
-    //   targets: ['defaults', 'not IE 11']
-    // }),
   ],
   build: {
     // 浏览器兼容性 ‘esnext’ | 'modules'
