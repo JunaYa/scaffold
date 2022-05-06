9ad863ac3210ae22c22b3e0e000d6f052ec42a14
YajunWang <wangyajun@hetao101.com>
2022-05-06 16:38:22 +0800

    feat: add css

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
 </script>
 
 <template>
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+  <button class="bg-sky-600 hover:bg-sky-700 ...">
+    Save changes
+  </button>
+  <button class="bg-sky-600 dark:bg-sky-900 hover:bg-sky-700 ...">
+    Save changes
+  </button>
 </template>
 
 <style>
